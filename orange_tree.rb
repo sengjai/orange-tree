87156f04434780d9d38870a0a729a531be3b7d2c
Soon Seng <soonseng.ee@thetechyhub.com>
2016-09-13 14:16:24 +0800

    Done

--- orange_tree.rb
+++ orange_tree.rb
@@ -1,6 +1,3 @@
-# This is how you define your own custom exception classes
-require 'byebug'
-
 class NoOrangesError < StandardError
 end
 
@@ -9,102 +6,72 @@ class OrangeTree
   attr_reader :height
   attr_reader :oranges
 
-  def initialize (height=0,age=0,oranges=5)
-    @oranges = oranges #start with 5 oranges
+  def initialize (height=0,age=0)
+    @oranges = []
     @height = height #height of tree
     @age = age #age of tree
-    @dead = false
   end
 
-  # Ages the tree one year
   def age!
-    basket = []
     #incrementing age
-    if @age <= 10
-      @age = @age + 1
-    else
-      @dead = dead?() #die at age of 10
-    end
-
-    #incrementing height
-    if @height >= 20
-      #stop growing
-    else
-      height()
-    end
-
-    #incremeting oranges
-    if @dead == false
-
-      @oranges = @oranges + rand(10) #increase 5 oranges
-      basket << @oranges
-    else
-      # say no more fruits
-      @previous_oranges = @oranges
-      @oranges = 0
-    end
-
-    #pick an orange at age of 5
-    if @age >= 5 
-      pick_an_orange!
-    end
+    @age += 1
+    @height += rand(1..5).ceil if age < 5
+    rand(1..10).times { @oranges << Orange.new(rand(1..3)).diameter } if @age > 5
   end
 
-  # Return the tree's current height
-  def height
-    @height = @height + 10
+  def any_oranges?
+    return @oranges == [] ? false : true
   end
 
-  # Returns the tree's current age
-  # def age
-  #   return @age
-  # end
+  def pick_an_orange!
+     raise NoOrangesError, "This tree has no oranges" unless self.any_oranges?
+
+     # orange-picking logic goes here
+     @oranges.shift #delete an orange
+  end
 
   def dead?
-    return true
-  end
-  # Returns +true+ if there are any oranges on the tree, +false+ otherwise
-  def any_oranges?
-    if @oranges <= 0
-      return true
-    else
-      return false
-    end
+    return @age > 10 ? true : false
   end
 
-  # Returns an Orange if there are any
-  # Raises a NoOrangesError otherwise
-  def pick_an_orange!
-    raise NoOrangesError, "This tree has no oranges" unless self.any_oranges?
-
-    # orange-picking logic goes here
-
-  end
+ 
 end
 
 class Orange
   attr_reader :diameter
   # Initializes a new Orange with diameter +diameter+
-  def initialize(diameter=0)
+  def initialize(diameter)
     @diameter = diameter
-    diameter_rand()
-  end
-
-  def diameter_rand()
-    @diameter = rand(10)
   end
 end
 
 tree = OrangeTree.new
 
-p tree.age! until tree.any_oranges?
+tree.age! until tree.any_oranges?
 
 puts "Tree is #{tree.age} years old and #{tree.height} feet tall"
 
-
-
-
-
-
-
-
+  until tree.dead?
+    basket = []
+  
+  
+    while tree.any_oranges?
+      basket << tree.pick_an_orange!
+    end
+  
+    if basket != []
+      avg_diameter = ( basket.reduce(:+).to_f / basket.size ).round(2)
+    else
+      avg_diameter = 0
+    end
+  
+    puts "Year #{tree.age} Report"
+    puts "Tree height: #{tree.height} feet"
+    puts "Harvest: #{basket.size} oranges with an average diameter of #{avg_diameter} inches"
+    puts ""
+  
+  
+    tree.age!
+  end
+  
+  puts "Alas, the tree, she is dead!"
